7b7c04277d654abb00e44b42e0750e4d63410903
zer0b2 <hvk@udpauto.ru>
2024-05-12 20:06:20 +0300

    init comm

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Baza from '@/components/Baza.vue'
+</script>
+<template>
+  <body id="app">
+    <Baza />
+  </body>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
